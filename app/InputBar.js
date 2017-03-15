720034ab31d27694e68d5660d6d2cf6f9b1ba5b1
Chad Marciniak <cmarciniak80@gmail.com>
2017-03-15 02:32:02 -0400

    fixed formatting

--- app/InputBar.js
+++ app/InputBar.js
@@ -5,23 +5,28 @@ class InputBar extends React.Component {
 	constructor(props) {
 		super();
 	}
+
 	inputChanged = (event) => {
 		this.setState({ message: event.target.value});
 		this.props.onTyping(event.target.value);
 	}
+
 	inputClicked = (event) => {
 		this.props.onMessageSubmit(event);
 		this.refs.messageInput.value = '';
 	}
+
 	keyPressed = (event) => {
 		if (event.keyCode === 13) {
 			event.preventDefault();
 			this.inputClicked();
 		}
 	}
+
 	inputFocus = (event) => {
 	 console.log(event);
 	}
+	
 	render() {
 		return (
 			<div id="InputBar">
